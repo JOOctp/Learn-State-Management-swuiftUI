e4c43e783905e453595ba09fed8d9c2e65cd445f
Jevon Putra <jevonop88@gmail.com>
2024-09-10 20:26:11 +0700

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Jevon Putra on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
